52267b15d16f3552f8fa5c0f28bb10d4936e0a5c
DVDetroit <115680871+DVDetroit@users.noreply.github.com>
2025-01-23 11:34:31 -0500

    Add files via upload

---
+++ graphical_landscape.py
@@ -0,0 +1,36 @@
+'''
+
+    Use turtle graphics and Python to create a graphical landscape. 
+
+    1) The bottom half of the screen should be green.
+
+    2) The top half of the screen should be blue.
+
+    3) Draw grey mountains in the background using triangles.  Create at least 3 mountains of varying sizes.
+
+    4) Draw an orange truck with black tires in the foreground.
+
+    5) Draw your initials in the bottom right corner using forward(), right(), left(), penup() and pendown().  Do not use the write command.
+
+    6) Include these instructions as comments at the beginning of your program.  Also comment each section, i.e. draw mountain, draw truck, etc.
+
+    Attach your .py file to Blackboard.  Make sure you attach your .py file and not the .pyproj file.  If you attach the wrong file you will get zero credit.
+
+    Don't forget the instructions as comments as well as code comments or you will loose points.
+'''
+
+import turtle
+turtle.setup(640, 480)
+#Creating the bottom half and painting it green
+
+
+#Creating the sky and making it blue
+
+
+#Drawing the mountains and using fill in to make them grey
+
+
+#Drawing the truck and painting it orange with black tires.
+
+
+#Drawing my initials
